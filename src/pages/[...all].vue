c12cf0c245de9ca9359986e0e557a694c6264504
Olivier Melcher <olivier.melcher@gmail.com>
2021-06-29 17:27:06 -0500

    initial commit

---
+++ src/pages/[...all].vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { useI18n } from 'vue-i18n'
+import { useRouter } from 'vue-router'
+
+const { t } = useI18n()
+
+const router = useRouter()
+
+router.replace({ query: { baz: 'bim' } })
+</script>
+
+<template>
+  <div>
+    {{ t('not-found') }}
+  </div>
+</template>
+
+<route lang="yaml">
+meta:
+  layout: 404
+</route>
